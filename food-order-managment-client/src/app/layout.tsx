4acc765a467a7daf1a691fb11e9a6c22435913a9
Dashan Nadeema <dashan.nadeema@tecciance.com>
2025-01-02 23:23:33 +0530

    font issue fix

--- food-order-managment-client/src/app/layout.tsx
+++ food-order-managment-client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Poppins } from "next/font/google";
 import "./globals.css";
-import { Poppins } from "@next/font/google";
+
 
 
 const geistSans = Geist({
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${poppins.variable} antialiased var(--font-poppins)`}
+        className={`${geistSans.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
