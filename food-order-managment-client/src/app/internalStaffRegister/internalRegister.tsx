a4a65b88a1960313657c63e24c32ecae2676e8d3
Dhaneera <wickramasuriyashehan@gmail.com>
2025-02-08 23:59:19 +0530

    register completed with all the required component

---
+++ food-order-managment-client/src/app/internalStaffRegister/internalRegister.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const registerInternalStaff = async(payload:any) => {
+    try {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/save-internal`,payload
+
+        ,{
+            withCredentials:true,
+            headers:{
+                "Content-Type":"application/json",
+            },
+        }
+    )
+        return response.data;
+        
+    } catch (error: any) {
+        console.error("Error:", error);
+        if (error.response) {
+            console.error("Error Status:", error.response.status);
+            console.error("Error Response Data:", error.response.data);
+        } else if (error.request) {
+            console.error("No Response Received:", error.request);
+        } else {
+            console.error("Request Error:", error.message);
+        }
+        throw error;
+        
+    }
+}
+export default registerInternalStaff;
